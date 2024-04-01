c877d89dec78f9d1aaee33609a325d3d0bacfeac
llu <97762571+6voo@users.noreply.github.com>
2024-04-01 13:29:58 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# streamable-site-thing
+made this for fun, i dont intend  on breaking any TOS with it.
+-
+# How to Use
+1. Clone the repository or download the script. 
+```
+git clone https://github.com/6voo/streamable-site-thing-main.git
+```
+
+2. change directory to the file
+```cd streamable-site-thing```
+
+3. Install the required packages
+```pip install -r requirements.txt```
+
+4. Run the script
+```python main.py```
+
+
+just wait for a valid link and see what someone else uploaded.
+made using web scraping
---
+++ main.py
@@ -0,0 +1,125 @@
+import random
+import requests
+import string
+import os
+from bs4 import BeautifulSoup
+from colorama import Fore
+from os import system, name
+
+working = []
+
+link = "https://streamable.com/"
+length = 6
+ctTag = 'h1'
+ctText = 'Oops!'
+characterList = string.ascii_lowercase + string.digits
+workingFile = open('working.txt', 'a')
+
+def mainLogo():
+    print(Fore.MAGENTA)
+    print("""
+                                                                                          ,-,
+                                                                                    _.-=;~ /_
+                                                                                _-~   '     ;.
+                                                                            _.-~     '   .-~-~`-._
+                                                                        _.--~~:.             --.____88
+                                                    ____.........--~~~. .' .  .        _..-------~~
+                                            _..--~~~~               .' .'             ,'
+                                        _.-~                        .       .     ` ,'
+                                    .'                                    :.    ./
+                                    .:     ,/          `                   ::.   ,'
+                                .:'     ,(            ;.                ::. ,-'
+                                .'     ./'.`.     . . /:::._______.... _/:.o/
+                                /     ./'. . .)  . _.,'               `88;?88|
+                            ,'  . .,/'._,-~ /_.o8P'                  88P ?8b
+                            _,'' . .,/',-~    d888P'                    88'  88|
+                        _.'~  . .,:oP'        ?88b              _..--- 88.--'8b.--..__
+                        :     ...' 88o __,------.88o ...__..._.=~- .    `~~   `~~      ~-._ Seal _.
+                        `.;;;:='    ~~            ~~~                ~-    -       -   -  
+                                              @llumix on discord                                                                         
+""")
+
+def endLogo():
+    print(Fore.MAGENTA)
+    print("""
+            _,-='"-.__               /\_/\\
+            `-.}       `=._,.-==-._.,  @ @._,
+                `-.__   _,-.   )       _,.-'
+                    `"     G..m-"^m`m'     BYE BYE! Click ENTER to end.   
+""")
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+def checkForCt(url, content_tag, content_text):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        element = soup.find(f"{content_tag}.{content_text}")
+
+        #  the entire html for inspection
+        soup.prettify()
+
+        # chek if the element is present and contains the expected text
+        if element and content_text.lower() in element.text.strip().lower():
+            # print("Username unavailable")
+            return False
+        else:
+            # print("Username available")
+            return True
+    except requests.exceptions.RequestException as e:
+        # print(f"Error: {e}")
+        return False
+
+def add_keyword(text, keyword):
+    return f"{keyword}{text}"
+
+def generateCode():
+    password = []
+    for i in range(6):
+        random_char = random.choice(characterList)
+        password.append(random_char)
+        code = ''.join(password)
+        final_link = add_keyword(code, link)
+        idk = checkForCt(final_link, ctTag, ctText)
+
+    if idk:
+        print(Fore.GREEN)
+        print("[Valid]", final_link)
+        working.append(final_link)
+
+        workingFile.write(str(final_link))
+        workingFile.write('\n')
+        workingFile.close()
+    else:
+        print(Fore.RED)
+        print("[Invalid] ", final_link) 
+
+clear()
+mainLogo()
+print(Fore.YELLOW)
+
+amount = int(input("Amount of Streamable links to generate: "))
+
+for _ in range(amount):
+    generateCode()
+
+print(Fore.YELLOW)
+print("             WORKING")
+print("            ---------")
+print("\n".join(working))
+
+input()
+clear()
+
+endLogo()
+input()
+clear()
+
+workingFile.close()
+os.close()
